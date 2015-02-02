0c6295d42e2b368fcba9621cce1f95bdde665443
astraw38 <Ashley.Straw@safenet-inc.com>
2015-02-02 09:54:41 -0500

    Changed version #

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.1',
     packages=['lint', 'lint/utils', 'lint/validators', 'lint/linters'],
     url='https://github.com/astraw38/lint',
-    download_url='https://github.com/astraw38/lint/tarball/1.1',
+    download_url='https://github.com/astraw38/lint/tarball/1.2',
     license='GNU GPL 2.0',
     author='Ashley Straw',
     author_email='strawac1@gmail.com',
