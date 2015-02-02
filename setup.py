3ee412809ed9206ca7d8ca06d84ed5361f64f7d3
astraw38 <Ashley.Straw@safenet-inc.com>
2015-02-02 10:15:40 -0500

    Upped version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='lint',
-    version='1.2',
+    version='1.2.1',
     packages=['lint', 'lint/utils', 'lint/validators', 'lint/linters'],
     url='https://github.com/astraw38/lint',
-    download_url='https://github.com/astraw38/lint/tarball/1.2',
+    download_url='https://github.com/astraw38/lint/tarball/1.2.1',
     license='GNU GPL 2.0',
     author='Ashley Straw',
     author_email='strawac1@gmail.com',
